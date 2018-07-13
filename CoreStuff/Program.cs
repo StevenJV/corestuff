bbb2c7a730d1121485c724bd1e66519a7789c2bc
StevenJV <steven@vorefamily.net>
2018-07-13 10:07:52 -0400

    Add project files.

---
+++ CoreStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
